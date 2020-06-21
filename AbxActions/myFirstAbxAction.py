10c27af29989005777da3265c3f289cfbab7bf54
Maher AlAsfar <malasfar@vmware.com>
2020-06-21 17:32:54 -0400

    Action Version: 5

--- AbxActions/myFirstAbxAction.py
+++ AbxActions/myFirstAbxAction.py
@@ -1,14 +1,22 @@
 
+import requests
 
 def handler(context, inputs):
+    
+    response = requests.get('https://raw.githubusercontent.com/vmwarelab/vra/master/AbxActions/properties/userDefinedVariableNumber.txt')
+    response.encoding = 'utf-8'
+    new_value = response.text.replace("\n","")
+    print(new_value)
+    old_userDefinedVariableNumber = inputs["customProperties"]["userDefinedVariableNumber"]
+    new_userDefinedVariableNumber = new_value
     
     
     outputs = {}
     
-    outputs["__metadata"] = inputs["__metadata"]
-    user_account = outputs["__metadata"]["userName"]
+    outputs["customProperties"] = inputs["customProperties"]
+    outputs["customProperties"]["userDefinedVariableNumber"] = new_userDefinedVariableNumber
     
-    print("User Account Name : " + user_account)
+    print("Setting userDefinedVariableNumber value from {0} to {1}".format(old_userDefinedVariableNumber, new_userDefinedVariableNumber))
     
     return outputs
 
