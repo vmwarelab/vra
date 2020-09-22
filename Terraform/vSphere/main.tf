4e2efcace0ff88bb9ee3ed8f64aefb740dc06eb9
Maher AlAsfar <malasfar@vmware.com>
2020-09-22 14:11:37 -0400

    vSphere Machine

--- Terraform/vSphere/main.tf
+++ Terraform/vSphere/main.tf
@@ -3,49 +3,64 @@ provider "vsphere" {
   #password       = var.vsphere_password
   #vsphere_server = var.vsphere_server
 
-variable "size" {
-  description = "VM Size"
-  default     = "small"
- }
-  
   # If you have a self-signed cert
   allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {
-  name = "BRAMPTON"
+  name = var.vsphere_datacenter
 }
 
 data "vsphere_datastore" "datastore" {
-  name          = "MGMT-LocalDisk1"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = var.vsphere_datastore
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_resource_pool" "pool" {
-  name          = "pre-prod-staging"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = var.vsphere_resource_pool
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_compute_cluster" "cluster" {
+  name          = var.vsphere_compute_cluster
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 data "vsphere_network" "network" {
-  name          = "DPortGroup-MGMT-Management"
-  datacenter_id = data.vsphere_datacenter.dc.id
+  name          = var.vsphere_network
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
+}
+
+data "vsphere_virtual_machine" "template" {
+  name          = var.vsphere_virtual_machine
+  datacenter_id = "${data.vsphere_datacenter.dc.id}"
 }
 
 resource "vsphere_virtual_machine" "vm" {
-  name             = "terraform-test"
-  resource_pool_id = data.vsphere_resource_pool.pool.id
-  datastore_id     = data.vsphere_datastore.datastore.id
+  name             = var.virtual_machine_name
+  resource_pool_id = "${data.vsphere_resource_pool.pool.id}"
+  datastore_id     = "${data.vsphere_datastore.datastore.id}"
 
-  num_cpus = 2
-  memory   = 1024
-  guest_id = "Ubuntu_Cloud_init_Template"
+  num_cpus = var.num_cpus
+  memory   = var.memory
+  guest_id = "${data.vsphere_virtual_machine.template.guest_id}"
+
+  scsi_type = "${data.vsphere_virtual_machine.template.scsi_type}"
 
   network_interface {
-    network_id = data.vsphere_network.network.id
+    network_id   = "${data.vsphere_network.network.id}"
+    adapter_type = "${data.vsphere_virtual_machine.template.network_interface_types[0]}"
   }
 
   disk {
-    label = "disk0"
-    size  = 20
+    label            = "disk0"
+    size             = "${data.vsphere_virtual_machine.template.disks.0.size}"
+    eagerly_scrub    = "${data.vsphere_virtual_machine.template.disks.0.eagerly_scrub}"
+    thin_provisioned = "${data.vsphere_virtual_machine.template.disks.0.thin_provisioned}"
   }
+
+  clone {
+    template_uuid = "${data.vsphere_virtual_machine.template.id}"
+
+    }
 }
