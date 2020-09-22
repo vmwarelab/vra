b9c1f00c8626feb18f2b4b75dc43ea03d0967b5b
Maher AlAsfar <malasfar@vmware.com>
2020-09-22 18:57:57 -0400

    Update variables.tf

--- Terraform/vSphere/variables.tf
+++ Terraform/vSphere/variables.tf
@@ -1,36 +1,37 @@
 
-variable "vsphere_user" {
-}
-
-variable "vsphere_password" {
-}
-
-variable "vsphere_server" {
-}
-
 variable "vsphere_datacenter" {
+  default = "BRAMPTON"
+  
 }
 
 variable "vsphere_datastore" {
+  default = "MGMT-LocalDisk1"
 }
 
-variable "vsphere_resource_pool" {
-}
+#variable "vsphere_resource_pool" {
+#}
 
 variable "vsphere_compute_cluster" {
+  default = "BRM-MGMT-CL"
 }
 
 variable "vsphere_network" {
+  default = "DPortGroup-MGMT-Management"
 }
 
 variable "vsphere_virtual_machine" {
+  default = "Ubuntu_Cloud_init_Template"
 }
 
 variable "num_cpus" {
+  default = "1"
+  
 }
 
 variable "memory" {
+  default = "1024"
 }
 
 variable "virtual_machine_name" {
+  default = "terraform-test"
 }
