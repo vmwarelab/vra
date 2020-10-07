c2d4adc64c1bc428c0aa8cd6031968c801fce512
Maher AlAsfar <malasfar@vmware.com>
2020-10-07 18:37:46 -0400

    aws-demo-01

---
+++ Terraform/aws/variables.tf
@@ -0,0 +1,16 @@
+#variable "AWS_ACCESS_KEY" {}
+
+#variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+  default = "ca-central-1"
+}
+
+variable "AMIS" {
+  type = map(string)
+  default = {
+    ca-central-1 = "ami-0431766daf444644c"
+    us-east-1 = "ami-02fb3fbca2bb793f1"
+    us-west-1 = "ami-0345db60aa8e3fa3d"
+  }
+}
