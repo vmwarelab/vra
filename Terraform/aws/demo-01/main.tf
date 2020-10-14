8d7c0f70817bca0714e8d9e377c47a66ddb79819
Maher AlAsfar <malasfar@vmware.com>
2020-10-14 19:32:44 -0400

    aws demo-01

--- Terraform/aws/demo-01/main.tf
+++ Terraform/aws/demo-01/main.tf
@@ -10,12 +10,3 @@ resource "aws_instance" "example" {
 variable "AWS_REGION" {
   default = "ca-central-1"
 }
-
-variable "AMIS" {
-  type = map(string)
-  default = {
-    ca-central-1 = "ami-0431766daf444644c"
-    us-east-1 = "ami-02fb3fbca2bb793f1"
-    us-west-1 = "ami-0345db60aa8e3fa3d"
-  }
-}
