08874873de54159120dc5d7aafe3bdfae68e49a7
Maher AlAsfar <malasfar@vmware.com>
2020-10-14 19:20:12 -0400

    aws backend demo

--- Terraform/aws/demo/main.tf
+++ Terraform/aws/demo/main.tf
@@ -6,3 +6,10 @@ resource "aws_instance" "Ubuntu_20_04" {
   ami           = "ami-0431766daf444644c"
   instance_type = "t2.micro"
 }
+
+terraform {
+  backend "s3" {
+    bucket = "terraform-state-vmwlab"
+    key = "terraform/demo4"
+  }
+}
