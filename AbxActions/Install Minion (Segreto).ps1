2094594377d7daa9bf3a1b25c5cf4f50cf86e662
Maher AlAsfar <malasfar@vmware.com>
2021-02-11 17:04:04 -0500

    Action Version: 2

--- AbxActions/Install Minion (Segreto).ps1
+++ AbxActions/Install Minion (Segreto).ps1
@@ -9,7 +9,7 @@ function Handler($context, $inputs) {
     $vrss = $context.getSecret($inputs.vrss) 
 
     $name = $inputs.resourceNames[0]
-    $hostname = $inputs.customProperties.hostname
+    $hostname = $inputs.resourceNames[0]
     
     Connect-VIServer $vcfqdn -User $vcuser -Password $vcpassword -Force
     write-host “Waiting for VM Tools to Start”
@@ -26,14 +26,21 @@ function Handler($context, $inputs) {
     
     if ($output.Equals($windowsString)) {
         $os_type = "Minion installed on Windows"
-        $script = "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope CurrentUser; Invoke-WebRequest https://repo.saltstack.com/windows/Salt-Minion-3002-Py3-x86-Setup.exe -UseBasicParsing -OutFile ~\Downloads\minion.exe"
+        $script = "[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12; Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force; Invoke-WebRequest https://repo.saltstack.com/windows/Salt-Minion-3002-Py3-x86-Setup.exe -UseBasicParsing -OutFile ~\Downloads\minion.exe"
         $script2 = "~\Downloads\minion.exe /S /master=$vrss /minion-name=$hostname"
+        
         Start-Sleep -s 60
+        Write-Host $tmpl_user
+        Write-Host $tmpl_pass
+        
         $runscript = Invoke-VMScript -VM $vm -ScriptText $script -GuestUser $tmpl_user -GuestPassword $tmpl_pass -ToolsWaitSecs 300
-        <#Write-Host $runscript.ScriptOutput#>
+        Write-Host $runscript.ScriptOutput
+        
         $runscript2 = Invoke-VMScript -VM $vm -ScriptText $script2 -GuestUser $tmpl_user -GuestPassword $tmpl_pass -ToolsWaitSecs 300
-        <#Write-Host $runscript2.ScriptOutput#>
+        Write-Host $runscript2.ScriptOutput
+        
         Write-Host "Minion installed on Windows using EXE"
+
     } else {
         $os_type = "Minion installed on Linux"
         $script = "echo $tmpl_pass | sudo -S curl -L https://bootstrap.saltstack.com -o install_salt.sh && echo $tmpl_pass | sudo -S hostnamectl set-hostname $hostname && echo $tmpl_pass | sudo -S chmod 777 install_salt.sh && echo $tmpl_pass | sudo -S sh install_salt.sh -A $vrss"
