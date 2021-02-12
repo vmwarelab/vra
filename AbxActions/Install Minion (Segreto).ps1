7c28e2a7f2b8c7482a0236d3f8f55dee62b90861
Maher AlAsfar <malasfar@vmware.com>
2021-02-12 00:06:24 -0500

    Action Version: 4

--- AbxActions/Install Minion (Segreto).ps1
+++ AbxActions/Install Minion (Segreto).ps1
@@ -19,25 +19,23 @@ function Handler($context, $inputs) {
     sleep 3
     } until ( $toolsStatus -eq ‘toolsOk’ )
     $vm = Get-vm -name $name
-    $output = $inputs.customProperties.softwareName
+    $output = $inputs.customProperties.osType
     Write-Host "VM OS Type is "$output
     
-    $windowsString = 'Microsoft Windows Server 2016 or later (64-bit)'
+    $windowsString = 'WINDOWS'
     
     if ($output.Equals($windowsString)) {
         $os_type = "Minion installed on Windows"
-        $script = "[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force; $ProgressPreference = 'SilentlyContinue'; Invoke-WebRequest https://repo.saltstack.com/windows/Salt-Minion-3002-Py3-x86-Setup.exe -UseBasicParsing -OutFile ~\Downloads\minion.exe"
+        $script = "[Net.ServicePointManager]::SecurityProtocol=[Net.SecurityProtocolType]::Tls12; $ProgressPreference = 'SilentlyContinue'; Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Force; Invoke-WebRequest https://repo.saltstack.com/windows/Salt-Minion-3002-Py3-x86-Setup.exe -UseBasicParsing -OutFile ~\Downloads\minion.exe"
         $script2 = "~\Downloads\minion.exe /S /master=$vrss /minion-name=$hostname"
         
         Start-Sleep -s 60
-        Write-Host $tmpl_user
-        Write-Host $tmpl_pass
-        
         $runscript = Invoke-VMScript -VM $vm -ScriptText $script -GuestUser $tmpl_user -GuestPassword $tmpl_pass -ToolsWaitSecs 300
-        Write-Host $runscript.ScriptOutput
+        <#Write-Host $runscript.ScriptOutput#>
         
+      
         $runscript2 = Invoke-VMScript -VM $vm -ScriptText $script2 -GuestUser $tmpl_user -GuestPassword $tmpl_pass -ToolsWaitSecs 300
-        Write-Host $runscript2.ScriptOutput
+        <#Write-Host $runscript2.ScriptOutput#>
         
         Write-Host "Minion installed on Windows using EXE"
 
@@ -45,9 +43,12 @@ function Handler($context, $inputs) {
         $os_type = "Minion installed on Linux"
         $script = "echo $tmpl_pass | sudo -S curl -L https://bootstrap.saltstack.com -o install_salt.sh && echo $tmpl_pass | sudo -S hostnamectl set-hostname $hostname && echo $tmpl_pass | sudo -S chmod 777 install_salt.sh && echo $tmpl_pass | sudo -S sh install_salt.sh -A $vrss"
         <#Write-Host $script#>
+        
+
         Start-Sleep -s 60
         $runscript = Invoke-VMScript -VM $vm -ScriptText $script -GuestUser $tmpl_user -GuestPassword $tmpl_pass -ToolsWaitSecs 300
         <#Write-Host $runscript.ScriptOutput#>
+        
         Write-Host = "Used bootstrap"
     }
 
